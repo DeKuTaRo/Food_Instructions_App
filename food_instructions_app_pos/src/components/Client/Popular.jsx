674c5e06a8f69fd23295f57ace704b938e7f82da
John5921 <leopard479579@gmail.com>
2024-01-12 00:14:27 +0700

    update home page

--- food_instructions_app_pos/src/components/Client/Popular.jsx
+++ food_instructions_app_pos/src/components/Client/Popular.jsx
@@ -1,54 +1,76 @@
-import { useEffect, useRef } from "react";
-import "@splidejs/react-splide/css";
-import "@splidejs/react-splide/css/skyblue";
-import "@splidejs/react-splide/css/sea-green";
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-
-import IconDango from "../../assets/icons/icon-dango.svg";
 import { IoIosArrowDropright } from "react-icons/io";
+import { Link } from "react-router-dom";
 function Popular() {
-  const cardRef = useRef(null);
-  useEffect(() => {
-    if (cardRef.current) {
-      const maxHeight = Math.max(
-        ...Array.from(cardRef.current.children).map((child) => {
-          return child.children[0].offsetHeight;
-        })
-      );
+  const [articles, setArticles] = useState([]);
 
-      Array.from(cardRef.current.children).forEach((child) => {
-        child.children[0].style.height = `${maxHeight}px`;
-      });
-    }
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const apiKey = "b56b11fadefc479ba229888bcf6d063e";
+        const response = await fetch(
+          `https://newsapi.org/v2/everything?q=Delicious cuisines of the world&apiKey=${apiKey}`
+        );
+        const data = await response.json();
+        setArticles(data.articles);
+      } catch (error) {
+        console.error("Error fetching articles:", error);
+      }
+    };
+
+    fetchArticles();
   }, []);
+
   return (
     <Box sx={{ margin: "3rem 0rem" }}>
-      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-        <div style={{ display: "flex", alignItems: "center", columnGap: "0.25rem", margin: "1rem 0" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            columnGap: "0.25rem",
+            margin: "1rem 0",
+          }}
+        >
           <Typography
             component="span"
             sx={{
-              backgroundImage: `url('${IconDango}')`,
               display: "block",
               width: 40,
               height: 40,
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-              backgroundSize: "contain",
               fill: "#D0021B",
-            }}></Typography>
-          <Typography ariant="h6" component="span" sx={{ fontWeight: "700", fontSize: "1.5rem" }}>
+              background: "url('YOUR_ICON_URL') center no-repeat",
+              backgroundSize: "contain",
+            }}
+          ></Typography>
+          <Typography
+            variant="h6"
+            component="span"
+            sx={{ fontWeight: "700", fontSize: "1.5rem" }}
+          >
             Latest Posts
           </Typography>
         </div>
-        <div style={{ display: "flex", alignItems: "center", justifyContent: "center", columnGap: "0.25rem" }}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            columnGap: "0.25rem",
+          }}
+        >
           <Typography component="span" sx={{ fontWeight: "700" }}>
             VIEW
           </Typography>{" "}
@@ -61,109 +83,87 @@ function Popular() {
           <IoIosArrowDropright />
         </div>
       </div>
-      <Grid container spacing={2} ref={cardRef}>
-        <Grid item xs={6} md={4}>
-          <Card sx={{ boxShadow: " 0 0 10px 0 rgba(50, 50, 50, .15)", backgroundColor: "white" }}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              image={
-                "https://www.justonecookbook.com/wp-content/uploads/2023/11/Akan-Mashu-National-Park-Lake-Kussharo-Travel-Guide-72-IMG_4602-400x267.jpg"
-              }
-            />
-            <CardContent sx={{ color: "black" }}>
-              <Typography variant="inherit" component="span">
-                Travel
-              </Typography>
-              <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: "700", marginTop: "0.5rem" }}>
-                Akan Mashu National Park – Lake Kussharo Travel Guide{" "}
-              </Typography>
-              <Typography variant="inherit" component="div" sx={{ margin: "0.5rem 0" }}>
-                Explore Akan Mashu National Park and Japan’s largest caldera lake – Lake Kussharo in…{" "}
-              </Typography>
-              <div style={{ display: "flex", alignItems: "center", columnGap: "0.25rem", color: "red" }}>
-                <Typography variant="overline" component="span" sx={{ fontWeight: "700" }}>
-                  VIEW
-                </Typography>{" "}
-                <Typography variant="overline" component="span" sx={{ fontStyle: "italic" }}>
-                  the
-                </Typography>{" "}
-                <Typography variant="overline" component="span" sx={{ fontWeight: "700" }}>
-                  POST
-                </Typography>
-                <IoIosArrowDropright />
-              </div>
-            </CardContent>
-          </Card>{" "}
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <Card sx={{ boxShadow: " 0 0 10px 0 rgba(50, 50, 50, .15)", backgroundColor: "white" }}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              image={
-                "https://www.justonecookbook.com/wp-content/uploads/2023/10/Japanese-Onion-Dressing-4004-I-400x267.jpg"
-              }
-            />
-            <CardContent sx={{ color: "black" }}>
-              <Typography variant="inherit" component="span">
-                Salad
-              </Typography>
-              <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: "700", marginTop: "0.5rem" }}>
-                A Million-Dollar Japanese Onion Dressing 玉ねぎドレッシング 0 (0){" "}
-              </Typography>
-              <Typography variant="inherit" component="div" sx={{ margin: "0.5rem 0" }}>
-                Sweet and simple Japanese Onion Dressing has an incredible depth of flavor from mild…{" "}
-              </Typography>
-              <div style={{ display: "flex", alignItems: "center", columnGap: "0.25rem", color: "red" }}>
-                <Typography variant="overline" component="span" sx={{ fontWeight: "700" }}>
-                  VIEW
-                </Typography>{" "}
-                <Typography variant="overline" component="span" sx={{ fontStyle: "italic" }}>
-                  the
-                </Typography>{" "}
-                <Typography variant="overline" component="span" sx={{ fontWeight: "700" }}>
-                  POST
-                </Typography>
-                <IoIosArrowDropright />
-              </div>
-            </CardContent>
-          </Card>{" "}
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <Card sx={{ boxShadow: " 0 0 10px 0 rgba(50, 50, 50, .15)", backgroundColor: "white" }}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              image={
-                "https://www.justonecookbook.com/wp-content/uploads/2023/09/Mille-Feuille-Nabe-2919-I-1-400x267.jpg"
-              }
-            />
-            <CardContent sx={{ color: "black" }}>
-              <Typography variant="inherit" component="span">
-                Soup + Stew
-              </Typography>
-              <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: "700", marginTop: "0.5rem" }}>
-                Mille-Feuille Nabe ミルフィーユ鍋{" "}
-              </Typography>
-              <Typography variant="inherit" component="div" sx={{ margin: "0.5rem 0" }}>
-                Enjoy my recipe for Japanese Mille-Feuille Nabe with “a thousand layers“ of napa cabbage…{" "}
-              </Typography>
-              <div style={{ display: "flex", alignItems: "center", columnGap: "0.25rem", color: "red" }}>
-                <Typography variant="overline" component="span" sx={{ fontWeight: "700" }}>
-                  VIEW
-                </Typography>{" "}
-                <Typography variant="overline" component="span" sx={{ fontStyle: "italic" }}>
-                  the
-                </Typography>{" "}
-                <Typography variant="overline" component="span" sx={{ fontWeight: "700" }}>
-                  POST
-                </Typography>
-                <IoIosArrowDropright />
-              </div>
-            </CardContent>
-          </Card>{" "}
-        </Grid>
+      <Grid container spacing={2}>
+        {articles.slice(0, 3).map((article, index) => (
+          <Grid key={index} item xs={4}>
+          <Link
+              to={`/detail/${index}`}
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <Card
+                sx={{
+                  boxShadow: "0 0 10px 0 rgba(50, 50, 50, .15)",
+                  backgroundColor: "white",
+                  height: "30em",
+                  display: "flex",
+                  flexDirection: "column",
+                  transition: "box-shadow 0.3s", // Add transition for smooth effect
+                  "&:hover": {
+                    boxShadow: "0 0 20px 0 rgba(50, 50, 50, .3)", // Adjust the shadow on hover
+                  },
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  alt={article.title}
+                  image={article.urlToImage}
+                  height={"20 rem"}
+                  sx={{
+                    flex: "1",
+                    height: "200px", // Chiều cao mặc định cho hình ảnh
+                    objectFit: "cover", // Đảm bảo hình ảnh không bị méo khi được thu nhỏ
+                    aspectRatio: "16/9",
+                  }}
+                />
+                <CardContent
+                  sx={{
+                    color: "black",
+                    maxHeight: "200px",
+                    overflow: "hidden",
+                    flex: "1",
+
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <Typography
+                    variant="inherit"
+                    component="span"
+                    sx={{ overflow: "hidden", textOverflow: "ellipsis" }}
+                  >
+                    {article.author}
+                  </Typography>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    sx={{
+                      fontWeight: "700",
+                      marginTop: "0.5rem",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      whiteSpace: "nowrap",
+                    }}
+                  >
+                    {article.title}
+                  </Typography>
+                  <Typography
+                    variant="inherit"
+                    component="div"
+                    sx={{
+                      margin: "0.5rem 0",
+                      maxHeight: "70px", // Chiều cao tối đa cho nội dung quảng cáo
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                    }}
+                  >
+                    {article.description}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Link>
+          </Grid>
+        ))}
       </Grid>
     </Box>
   );
